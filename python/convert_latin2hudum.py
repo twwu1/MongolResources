939a8a7625fe7acec9e5b0818ca8cc8f8e18df4e
twwu1 <112338803+twwu1@users.noreply.github.com>
2024-05-31 13:46:27 -0400

    Update convert_latin2hudum.py
    Added procedure for removing trailing spaces on each line; accounted for punctuation in MVS cases; specified encoding in file open() function; added more detail in comments;

--- python/convert_latin2hudum.py
+++ python/convert_latin2hudum.py
@@ -9,7 +9,10 @@
 #A modified form of the German keyboard or Turkish keyboard might be best for typing these letters.
 #Modified keyboards could be made with Keyman, at https://keyman.com/developer/
 
-#Unicode has the same code for both the back vowel g sound and the front vowel g sound, so this python program only uses the letter 'g' to represent both sounds; likewise for q. 
+#Unicode has the same code for both the back vowel g sound and the front vowel g sound, so this python program only uses the letter 'g' to represent both sounds; likewise for q.
+#The character 'U+183A' (like a backwards 'C' with two or three prongs sticking out of its upper tip) was borrowed directly from the old Uyghur alphabet, and originally served as the front vowel K in the medieval Mongol alphabet, but then in later times it came to be only used for foreign loanwords.
+#Outside of foreign loanwords, the front vowel K came to be written with only one prong on its upper tip; this character has the same unicode as the back vowel q.
+ 
 #For noun case endings, a hyphen '-', which is converted to NNBSP (U+202f), must be in between the noun and the case ending for it to be rendered properly.
 #In order to be rendered properly, the resulting hudum text must be printed in font styles dedicated to the traditional Mongol script.
 
@@ -36,7 +39,7 @@
 
 #Please save the Romanized text in a file called romanized_file.txt and store it next to this python file within the same folder.
 
-f = open('romanized_file.txt', 'r')
+f = open('romanized_file.txt', 'r', encoding="utf8")
 text = f.read()
 f.close()
 
@@ -45,6 +48,7 @@ dict = {'g': '\u182D', 'q': '\u182C', 't': '\u1832', 'd': '\u1833', 's': '\u1830
         #Foreign Sounds
         'ē':'\u1827', 'p': '\u182B', 'f':'\u1839', 'k': '\u183A', 'ǩ':'\u183B', '\u02A6': '\u183C', 'z': '\u183D', 'h': '\u183E', 'ẑ': '\u183F', 'ł': '\u1840', 'ĵ':'\u1841', 'ĉ': '\u1842'
         }
+letter_inventory = dict.keys()
 #The hyphen '-' is always converted to NNBSP
 punctuation_dict = {'-': '\u202f', '....': '\u1801', ',': '\u1802', ';': '\u1803', ':': '\u1804'}
 punctuations = punctuation_dict.keys()
@@ -55,6 +59,22 @@ MVS_exceptions = {'ene': '', 'tere': ''}.keys()
 hudum_chars = dict.keys()
 vowels = {'a', 'o', 'u', 'e', 'ü', 'ö', 'i', 'ē'}
 
+#remove trailing whitespace in each line
+lines = text.split('\n')
+text = ""
+num_lines = len(lines)
+for i in range(0, num_lines):
+    line = lines[i]
+    line_len = len(line)
+    if line_len > 0:
+        line.strip(' ')
+        line.strip('\t')
+        line_len = len(line)
+        if i < num_lines - 1 or line_len > 0:
+            text = text + line + '\n'
+
+#split text into array of words
+#each newline character is treated as a separate word
 text = text.replace('\n', ' \n ')
 words = text.split(' ')
 
@@ -68,7 +88,7 @@ for word in words:
     word_beginning_index = 0
     isRecent_loanword = False
 
-    if word[0] == '*': #if the first character of word is an asterix
+    if word_len > 0 and word[0] == '*': #if the first character of word is an asterix
         isRecent_loanword = True
         word_beginning_index = 1
 
@@ -76,7 +96,7 @@ for word in words:
         char = word[i]
         if char in hudum_chars:
             
-            last_char = i == word_len - 1
+            last_char = i == word_len - 1 or not word[i + 1] in letter_inventory
             follows_MVS_cons = prev_char in MVS_consonants and (char == 'a' or char == 'e')
             isMVSexception = word in MVS_exceptions
 
@@ -107,9 +127,9 @@ for word in words:
             elif char == 'o' or char == 'u' and i == word_len - 1:
                 output = output + '\u180B'
     
-    if not word.isspace():
+    if word_len > 0 and not word.isspace():
         output = output + " "
 
-output_file = open('hudum_file.txt', 'w')
+output_file = open('hudum_file.txt', 'w', encoding="utf8")
 output_file.write(output)
 output_file.close()
